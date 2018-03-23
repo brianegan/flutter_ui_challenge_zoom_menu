4e97ffc76a386188a72041cc041e53286011228f
Matt Carroll <me@mattcarroll.name>
2018-03-22 23:32:53 -0700

    Pulled out top level menu coordination into zoom_scaffold.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zoom_menu/menu.dart';
-import 'package:zoom_menu/prototools.dart';
 import 'package:zoom_menu/restaurant_list_screen.dart';
-import 'package:zoom_menu/underneath_menu_screen.dart';
+import 'package:zoom_menu/zoom_scaffold.dart';
 
 void main() => runApp(new MyApp());
 
@@ -79,78 +78,12 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-
-  double menuOpenPercent = 0.0;
-  bool isMenuOpen = false;
-  bool isMenuOpening = true;
-
-  AnimationController openMenuController;
-
-  final InnerAnimationCurve contentZoomOutSubset = new InnerAnimationCurve(0.0, 0.2);
-  final InnerAnimationCurve contentSlideOutSubset = new InnerAnimationCurve(0.1, 0.3);
-
-  final InnerAnimationCurve contentZoomAndSlideInSubset = new InnerAnimationCurve(0.0, 0.3);
-
-  _MyHomePageState() {
-    openMenuController = new AnimationController(duration: const Duration(milliseconds: 1000), vsync: this)
-        ..addListener(() {
-          setState(() {
-            menuOpenPercent = openMenuController.value;
-          });
-        });
-  }
-
-  toggle() {
-    if (isMenuOpen) {
-      isMenuOpen = false;
-      isMenuOpening = false;
-      openMenuController.reverse(from: 1.0);
-    } else {
-      isMenuOpen = true;
-      isMenuOpening = true;
-      openMenuController.forward(from: 0.0);
-    }
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final zoomOutPercent = isMenuOpening
-        ? Curves.easeOut.transform(
-            contentZoomOutSubset.transform(menuOpenPercent)
-          )
-        : 1.0 - Curves.easeOut.transform(
-            contentZoomAndSlideInSubset.transform(1.0 - menuOpenPercent)
-          );
-    final scale = 1.0 - (0.2 * zoomOutPercent);
-
-    final slidePercent = isMenuOpening
-      ? Curves.easeOut.transform(
-          contentSlideOutSubset.transform(menuOpenPercent)
-        )
-      : 1.0 - Curves.easeOut.transform(
-          contentZoomAndSlideInSubset.transform(1.0 - menuOpenPercent)
-        );
-    final horizontalOffset = 250.0 * slidePercent;
-
     return new Scaffold(
-      body: new Stack(
-        children: [
-          new UnderneathMenuScreen(
-            menuOpenPercent: menuOpenPercent,
-            isMenuOpening: isMenuOpening,
-          ),
-          new Transform(
-            transform: new Matrix4.identity()
-                ..scale(scale, scale, 1.0)
-                ..translate(horizontalOffset, 0.0, 0.0),
-            alignment: Alignment.center,
-            child: new RestaurantListScreen(
-              onMenuTap: toggle,
-              isZoomedOut: menuOpenPercent > 0.0,
-            ),
-          ),
-        ]
+      body: new ZoomScaffold(
+
       ),
     );
   }
