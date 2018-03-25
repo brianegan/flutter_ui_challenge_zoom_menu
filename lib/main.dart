7be78c470310b6021cb4b3931bcd3166258f2e4d
Matt Carroll <me@mattcarroll.name>
2018-03-24 21:31:30 -0700

    Implemented single card in content screen.

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,102 @@ class _MyHomePageState extends State<MyHomePage> {
             }
           ),
         ),
-        body: new Container(),
+        body: new ListView(
+          children: [
+            new Theme(
+              data: new ThemeData(
+                brightness: Brightness.light,
+              ),
+              child: new Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+                child: new Card(
+                  elevation: 10.0,
+                  child: new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      new Image.asset(
+                        'assets/steak_on_cooktop.jpg',
+                        width: double.infinity,
+                        height: 150.0,
+                        fit: BoxFit.cover,
+                      ),
+                      new Row(
+                        children: [
+                          new Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: new Container(
+                              padding: const EdgeInsets.all(10.0),
+                              decoration: new BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: new BorderRadius.all(const Radius.circular(15.0)),
+                              ),
+                              child: new Icon(
+                                Icons.fastfood,
+                                color: Colors.white,
+                              )
+                            ),
+                          ),
+                          new Expanded(
+                            child: new Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                new Text(
+                                  'il domacca',
+                                  style: const TextStyle(
+                                    fontSize: 25.0,
+                                    fontFamily: 'mermaid',
+                                  ),
+                                ),
+                                new Text(
+                                  '78 5TH AVENUE, NEW YORK',
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'bebas-neue',
+                                    letterSpacing: 1.0,
+                                    color: const Color(0xFFAAAAAA),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          new Container(
+                            width: 2.0,
+                            height: 70.0,
+                            decoration: new BoxDecoration(
+                              gradient: new LinearGradient(
+                                colors: [
+                                  Colors.white,
+                                  Colors.white,
+                                  const Color(0xFFAAAAAA),
+                                ],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                              ),
+                            ),
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                            child: new Column(
+                              children: [
+                                new Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.red,
+                                ),
+                                new Text(
+                                  '84',
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
