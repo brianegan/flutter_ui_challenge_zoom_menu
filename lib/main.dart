4661a65416c3a6f7ac34a54370d3e65f07b64e74
Matt Carroll <me@mattcarroll.name>
2018-03-24 23:01:45 -0700

    Applied static scale and transform to content screen.

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  Widget _scaleAndPositionContentScreen(contentScreen) {
+    return new Transform(
+      transform: new Matrix4
+          .translationValues(250.0, 0.0, 0.0)
+        ..scale(0.85, 0.85),
+      alignment: Alignment.centerLeft,
+      child: Container(
+        decoration: new BoxDecoration(
+          boxShadow: [
+            new BoxShadow(
+              color: const Color(0x44000000),
+              offset: const Offset(0.0, 5.0),
+              blurRadius: 20.0,
+              spreadRadius: 10.0,
+            )
+          ],
+        ),
+        child: new ClipRRect(
+          borderRadius: new BorderRadius.circular(10.0),
+          child: contentScreen,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Material(
       child: Stack(
         children: [
           new MenuScreen(),
-//        new RestaurantScreen(),
+          _scaleAndPositionContentScreen(new RestaurantScreen()),
         ],
       ),
     );
