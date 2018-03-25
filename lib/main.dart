0dcce4305e904a25e403b5cf39a261011a626fb3
Matt Carroll <me@mattcarroll.name>
2018-03-24 22:49:58 -0700

    Extracted RestaurantScreen to own file. Created static menu screen in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_menu/restaurant_screen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -24,170 +25,97 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: new BoxDecoration(
-        image: new DecorationImage(
-          image: new AssetImage('assets/wood_bk.jpg'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: new Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: new AppBar(
-          title: new Text('THE PALEO PADDOCK'),
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          leading: new IconButton(
-              icon: new Icon(
-                Icons.menu,
+    return new Material(
+      child: Stack(
+        children: [
+          new Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage('assets/dark_grunge_bk.jpg'),
+                fit: BoxFit.cover,
               ),
-              onPressed: () {
-
-              }
+              color: Colors.transparent,
+            ),
+            child: new Stack(
+              children: [
+                new Transform(
+                  transform: new Matrix4.translationValues(-100.0, 0.0, 0.0),
+                  child: new OverflowBox(
+                    maxWidth: double.infinity,
+                    alignment: Alignment.topLeft,
+                    child: new Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: new Text(
+                        'Menu',
+                        style: new TextStyle(
+                          color: const Color(0x88444444),
+                          fontSize: 240.0,
+                          fontFamily: 'mermaid',
+                        ),
+                        textAlign: TextAlign.left,
+                        softWrap: false,
+                      ),
+                    ),
+                  ),
+                ),
+                new Transform(
+                  transform: new Matrix4.translationValues(0.0, 250.0, 0.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      new _MenuItem(
+                        title: 'THE PADDOCK',
+                        isSelected: true,
+                      ),
+                      new _MenuItem(
+                        title: 'THE HERD',
+                        isSelected: false,
+                      ),
+                      new _MenuItem(
+                        title: 'HELP US GROW',
+                        isSelected: false,
+                      ),
+                      new _MenuItem(
+                        title: 'SETTINGS',
+                        isSelected: false,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        body: new ListView(
-          children: [
-            new _RestaurantCard(
-              headerImageAssetPath: 'assets/eggs_in_skillet.jpg',
-              icon: Icons.fastfood,
-              iconBackgroundColor: Colors.orange,
-              title: 'il domacca',
-              subtitle: '78 5TH AVENUE, NEW YORK',
-              heartCount: 84,
-            ),
-            new _RestaurantCard(
-              headerImageAssetPath: 'assets/steak_on_cooktop.jpg',
-              icon: Icons.local_dining,
-              iconBackgroundColor: Colors.red,
-              title: 'Mc Grady',
-              subtitle: '79 5TH AVENUE, NEW YORK',
-              heartCount: 84,
-            ),
-            new _RestaurantCard(
-              headerImageAssetPath: 'assets/spoons_of_spices.jpg',
-              icon: Icons.fastfood,
-              iconBackgroundColor: Colors.purpleAccent,
-              title: 'Sugar & Spice',
-              subtitle: '80 5TH AVENUE, NEW YORK',
-              heartCount: 84,
-            ),
-          ],
-        ),
+
+//        new RestaurantScreen(),
+        ],
       ),
     );
   }
 }
 
-class _RestaurantCard extends StatelessWidget {
+class _MenuItem extends StatelessWidget {
 
-  final String headerImageAssetPath;
-  final IconData icon;
-  final Color iconBackgroundColor;
-  final String title;
-  final String subtitle;
-  final int heartCount;
+  final title;
+  final isSelected;
 
-  _RestaurantCard({
-    this.headerImageAssetPath,
-    this.icon,
-    this.iconBackgroundColor,
+  _MenuItem({
     this.title,
-    this.subtitle,
-    this.heartCount,
+    this.isSelected,
   });
 
   @override
   Widget build(BuildContext context) {
-    return new Theme(
-      data: new ThemeData(
-        brightness: Brightness.light,
-      ),
-      child: new Padding(
-        padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-        child: new Card(
-          elevation: 10.0,
-          child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              new Image.asset(
-                headerImageAssetPath,
-                width: double.infinity,
-                height: 150.0,
-                fit: BoxFit.cover,
-              ),
-              new Row(
-                children: [
-                  new Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: new Container(
-                        padding: const EdgeInsets.all(10.0),
-                        decoration: new BoxDecoration(
-                          color: iconBackgroundColor,
-                          borderRadius: new BorderRadius.all(const Radius.circular(15.0)),
-                        ),
-                        child: new Icon(
-                          icon,
-                          color: Colors.white,
-                        )
-                    ),
-                  ),
-                  new Expanded(
-                    child: new Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        new Text(
-                          title,
-                          style: const TextStyle(
-                            fontSize: 25.0,
-                            fontFamily: 'mermaid',
-                          ),
-                        ),
-                        new Text(
-                          subtitle,
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            fontFamily: 'bebas-neue',
-                            letterSpacing: 1.0,
-                            color: const Color(0xFFAAAAAA),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  new Container(
-                    width: 2.0,
-                    height: 70.0,
-                    decoration: new BoxDecoration(
-                      gradient: new LinearGradient(
-                        colors: [
-                          Colors.white,
-                          Colors.white,
-                          const Color(0xFFAAAAAA),
-                        ],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                      ),
-                    ),
-                  ),
-                  new Padding(
-                    padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-                    child: new Column(
-                      children: [
-                        new Icon(
-                          Icons.favorite_border,
-                          color: Colors.red,
-                        ),
-                        new Text(
-                          '$heartCount',
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+    return new Padding(
+      padding: const EdgeInsets.only(left: 50.0, top: 15.0, bottom: 15.0),
+      child: new Text(
+        title,
+        style: new TextStyle(
+          color: isSelected ? Colors.red : Colors.white,
+          fontSize: 25.0,
+          fontFamily: 'bebas-neue',
+          letterSpacing: 2.0,
         ),
       ),
     );
