b95a8ba0bb0fe3b1a02dc265756e1357d1c5d3f6
Matt Carroll <me@mattcarroll.name>
2018-03-24 20:19:06 -0700

    Resetting code back to a blank slate.

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # zoom_menu_proto
 
 Flutter UI Challenge to create a menu that zooms content in and out.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](http://flutter.io/).
--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:zoom_menu/menu.dart';
-import 'package:zoom_menu/restaurant_list_screen.dart';
-import 'package:zoom_menu/zoom_scaffold.dart';
 
 void main() => runApp(new MyApp());
-
-final paleoPaddockMenu = new Menu(
-  menuItems: [
-    new MenuItem(
-      title: 'THE PADDOCK',
-      screenBuilder: new ScreenBuilder(
-        backgroundBuilder: (BuildContext context) {
-          return new Container();
-        },
-        contentBuilder: (BuildContext context) {
-          return new RestaurantListScreen(
-            isZoomedOut: false,
-            onMenuTap: () {},
-          );
-        },
-      ),
-    ),
-
-    new MenuItem(
-      title: 'THE HERO',
-      screenBuilder: new ScreenBuilder(
-        backgroundBuilder: (BuildContext context) {
-          return new Container();
-        },
-        contentBuilder: (BuildContext context) {
-          return new Container();
-        },
-      ),
-    ),
-
-    new MenuItem(
-      title: 'HELP US GROW',
-      screenBuilder: new ScreenBuilder(
-        backgroundBuilder: (BuildContext context) {
-          return new Container();
-        },
-        contentBuilder: (BuildContext context) {
-          return new Container();
-        },
-      ),
-    ),
-
-    new MenuItem(
-      title: 'SETTINGS',
-      screenBuilder: new ScreenBuilder(
-        backgroundBuilder: (BuildContext context) {
-          return new Container();
-        },
-        contentBuilder: (BuildContext context) {
-          return new Container();
-        },
-      ),
-    ),
-  ]
-);
 
 class MyApp extends StatelessWidget {
   @override
@@ -82,9 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new ZoomScaffold(
-
-      ),
+      body: new Container(),
     );
   }
 }
