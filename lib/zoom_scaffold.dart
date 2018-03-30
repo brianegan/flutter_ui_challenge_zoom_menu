8250c023cccdb12c6a7596d192f179da9b39001b
Matt Carroll <me@mattcarroll.name>
2018-03-29 21:21:48 -0700

    Implemented static zoomed out display of content. No menu control exists yet.

--- lib/zoom_scaffold.dart
+++ lib/zoom_scaffold.dart
@@ -18,31 +18,58 @@ class ZoomMenuScaffold extends StatefulWidget {
 class _ZoomMenuScaffoldState extends State<ZoomMenuScaffold> {
 
   _createContentDisplay() {
-    return Container(
-      decoration: new BoxDecoration(
-        image: widget.contentScreen.background,
-      ),
-      child: new Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: new AppBar(
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          elevation: 0.0,
-          leading: new IconButton(
-              icon: new Icon(Icons.menu),
-              onPressed: () {
-                // TODO: open/close the menu
-              }
+    return _zoomAndSlideContent(
+        Container(
+          decoration: new BoxDecoration(
+            image: widget.contentScreen.background,
           ),
-          title: new Text(
-            widget.contentScreen.title,
-            style: new TextStyle(
-              fontFamily: 'bebas-neue',
-              fontSize: 25.0,
+          child: new Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: new AppBar(
+              backgroundColor: Colors.transparent,
+              centerTitle: true,
+              elevation: 0.0,
+              leading: new IconButton(
+                  icon: new Icon(Icons.menu),
+                  onPressed: () {
+                    // TODO: open/close the menu
+                  }
+              ),
+              title: new Text(
+                widget.contentScreen.title,
+                style: new TextStyle(
+                  fontFamily: 'bebas-neue',
+                  fontSize: 25.0,
+                ),
+              ),
             ),
+            body: widget.contentScreen.contentBuilder(context),
           ),
         ),
-        body: widget.contentScreen.contentBuilder(context),
+    );
+  }
+
+  _zoomAndSlideContent(Widget content) {
+    return new Transform(
+      transform: new Matrix4
+        .translationValues(275.0, 0.0, 0.0)
+        ..scale(0.8, 0.8),
+      alignment: Alignment.centerLeft,
+      child: Container(
+        decoration: new BoxDecoration(
+          boxShadow: [
+            new BoxShadow(
+              color: const Color(0x44000000),
+              offset: const Offset(0.0, 5.0),
+              blurRadius: 20.0,
+              spreadRadius: 10.0,
+            ),
+          ],
+        ),
+        child: new ClipRRect(
+          borderRadius: new BorderRadius.circular(10.0),
+          child: content
+        ),
       ),
     );
   }
@@ -52,7 +79,7 @@ class _ZoomMenuScaffoldState extends State<ZoomMenuScaffold> {
     return Stack(
       children: [
         widget.menuScreen,
-//        _createContentDisplay(),
+        _createContentDisplay(),
       ],
     );
   }
